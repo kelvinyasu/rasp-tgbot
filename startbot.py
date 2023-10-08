e5cddc962938f89fa0252a53be6ea7a6ac597c14
kelvinyasu <yasu@omg.hk>
2023-10-08 10:00:50 +0800

    Update startbot.py

--- startbot.py
+++ startbot.py
@@ -7,7 +7,7 @@ from telepot.loop import MessageLoop
 from telegram_bot import read_bot_token, handle_command
 
 # Your script's version number
-SCRIPT_VERSION = "0.21"
+SCRIPT_VERSION = "0.3"
 
 # File to store chat IDs
 CHAT_ID_FILE = '/usr/local/bin/tgbot/chat_ids.txt'
